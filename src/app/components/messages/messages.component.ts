8009552c72e9ce0b795e5d93aa1ee6cdd1122e3f
paras18csu153 <paras18csu153@ncuindia.edu>
2022-02-11 10:30:54 +0530

    Added Send Message UI.

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -16,6 +16,10 @@ export class MessagesComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void{
     console.log(this.message);
+    var messages = document.getElementById("messages")!;
+    console.log(messages);
+    if(messages){
+      messages.innerHTML = messages.innerHTML + "<div class='row my-rows' style= 'margin: 0'><div class='offset-md-7 col-md-5'><p style='background-color: rgb(250, 250, 250, 0.5);margin-top: 5%;margin-bottom: 5%;padding: 5% 2.5%;'>"+ this.message +"</p></div></div>";
+    }
   }
-
 }
